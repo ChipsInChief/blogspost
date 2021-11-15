bcb180fe432cfb164df7624f8f3cdbd5525b7167
Benjamin GUILLAUME <ouri3l@gmail.com>
2021-11-15 20:29:21 +0100

    📦 NEW: Ajout des articles "issus" de la base de données (on fait semblant)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { title } from 'process';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'blogspost';
+  title = 'Mon super Blog !';
+  postsFromDatabase = [
+    { 
+      title: 'Mon premier article',
+      content: 'Deserunt magna consequat officia deserunt ipsum exercitation exercitation veniam velit proident esse. Nisi laboris tempor dolore nisi enim deserunt irure ad eu ex ut laborum labore. Voluptate sunt sit sit velit do eu aliqua ex laborum fugiat duis sit consequat nulla. Et velit velit tempor amet consectetur.',
+      loveIts: 10,
+      created_at: new Date()
+    },
+    { 
+      title: 'Mon deuxième article',
+      content: 'Nulla ullamco duis aute ad sunt occaecat dolor esse ullamco non consequat sit. Quis amet enim consequat eu nostrud. Laborum pariatur commodo non aute ex incididunt aute sit. Dolor labore in exercitation deserunt eu do duis duis. Reprehenderit non amet mollit quis aliquip cillum enim proident in velit nostrud occaecat. Veniam duis voluptate commodo cupidatat. Pariatur qui in ea nulla laborum ea magna ex ea mollit occaecat.',
+      loveIts: -2,
+      created_at: new Date()
+    },
+    { 
+      title: 'Mon troisième article',
+      content: 'Do aliqua non excepteur pariatur duis quis ullamco dolor excepteur est irure nulla. Ut Lorem proident sit laboris exercitation aute aute voluptate eiusmod nulla labore ullamco irure laboris. Ut aute laborum ut laboris deserunt. Cupidatat sit cupidatat velit dolore occaecat.',
+      loveIts: 0,
+      created_at: new Date()
+    }
+  ];
 }
