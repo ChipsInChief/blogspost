b65bab2ab529f758b275105bc9f1d0b5fcfe0270
Benjamin GUILLAUME <ouri3l@gmail.com>
2021-11-15 21:08:38 +0100

    📦 NEW: Plomberie v1 : Les données sont propagées depuis le tableau de AppComponent

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -7,7 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class PostListComponent implements OnInit {
 
-  @Input() posts = [];
+  @Input() posts?: any[];
 
   constructor() { }
 
