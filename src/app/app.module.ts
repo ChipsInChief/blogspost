d0ca893ea206574e3113b2592aabfd34779507ca
Benjamin GUILLAUME <ouri3l@gmail.com>
2021-11-22 19:33:29 +0100

    📦 NEW: Localisation en FR 😎

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { PostListComponent } from './post-list/post-list.component';
 import { PostListItemComponent } from './post-list-item/post-list-item.component';
+
+// Pour la localisation fr
+import { registerLocaleData } from '@angular/common';
+import france from '@angular/common/locales/fr';
+registerLocaleData(france);
 
 @NgModule({
   declarations: [
@@ -14,7 +19,9 @@ import { PostListItemComponent } from './post-list-item/post-list-item.component
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: "fr" }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
